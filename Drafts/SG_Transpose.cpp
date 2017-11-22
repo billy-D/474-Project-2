c72988b4af52018ea0d0f081dea98f4fe831bfee
BillyD <bdang12@gmail.com>
2017-11-22 10:48:53 -0800

    init_Push_2

--- Drafts/SG_Transpose.cpp
+++ Drafts/SG_Transpose.cpp
@@ -2,7 +2,6 @@
 Names: Billy Dang, Sean Mckean, Hassan Hamod
 File: SG_Transpose.cpp
 ----------------------------------------------*/
-
 #include <iostream>
 #include "Node.h"
 #include <mpi.h>
@@ -11,13 +10,115 @@ File: SG_Transpose.cpp
 // Function Declarations
 void printList(Node*);
 void insert(Node*&, int);
+void deleteList(Node*&);
 
 
 int main(){
+    //declare a pointer to use for list
+	NodeType * head = nullptr;
 
+	//used to pass a value into to add to list
+    int value = 0;
+
+    //menu choice
+	char choice = ' ';
+
+	//used to store the return value from find, assign it to be false by default
+    bool returnValue = false;
+    
+    do 
+	{
+		//list options
+		listOptions();
+
+		//get users choice based on menu
+		cin >> choice;
+
+		//convert lowercase to uppercase if any lowercase inputs
+		choice = toupper(choice);
+
+		//spaceing between menu and user interactions
+		cout << endl;
+
+		switch (choice) {
+		case 'A':
+			//insert a value at the top of the list as the head
+			cout << "Enter a value to insert: ";
+			cin >> value;
+
+			//call the insert function
+			insert(head, value);
+
+
+			break;
+		case 'P':
+			//check if the list is empty
+			if (head == nullptr)
+			{
+				cout << "List is empty at the moment...\n";
+			}
+			else
+			{
+				//print the linked list by calling the printList function
+				cout << "List of Nodes: Newest -> Oldest\n";
+				printList(head);
+			}
+			system("PAUSE");
+			system("CLS");
+			break;
+
+		case 'Q':
+			//quit
+			
+			if (head == nullptr)
+			{
+				cout << "List is empty, good to go!\n";
+			}
+			else
+			{
+				//print an error message
+				cout << "List still exist, but no worries calling deleteList function... " << endl;
+
+				//call deleteList again to kill list
+				deleteList(head);
+			}
+			
+			system("PAUSE");
+			system("CLS");
+			break;
+
+		default:
+			//error message if user enters something different
+			cout << "Invalid option!\n";
+			system("PAUSE");
+			system("CLS");
+			break;
+		}
+
+
+	} while (choice != 'Q');
+
+    cout << endl;
     return 0;
 }
 
+
+/*-------------------------------------------------------
+function: listOptions
+parameters: none
+purpose: provide user with a list of choices to use
+-------------------------------------------------------*/
+void listOptions()
+{
+	cout << "=============Menu=================\n";
+	cout << "A -> Add\n";
+	cout << "P -> Print\n";
+	cout << "Q -> Quit\n";
+	cout << "===================================\n";
+
+	cout << "Enter a valid option: ";
+
+}
 
 
 /*-------------------------------------------------------
@@ -43,8 +144,6 @@ void insert(Node*&head, int value)
 	head = currentPtr;
 
 	cout << value << " is added to the list!\n";
-	system("PAUSE");
-	system("CLS");
 }
 
 
@@ -70,5 +169,33 @@ void printList(Node*head)
 
 	//spacing
 	cout << endl;
+
+}
+
+
+/*-------------------------------------------------------
+function: deleteList
+parameters: A pointer that is a passed by reference 
+purpose: deleting every node in the linked list
+-------------------------------------------------------*/
+void deleteList(Node*&head)
+{
+
+	//Need a loop to iterate through each Node in the list
+	while (head != nullptr)
+	{
+
+		//Need a pointer to use to delete Node
+		Node*Temp = head;
+
+		//At the top of the list already, need to move the head pointer to the next node
+		head = head->nextPtr;
+
+		//Temp is still linked to the next node, cut its link to the rest of the list
+		Temp->nextPtr = nullptr;
+		delete Temp;
+
+		Temp = nullptr;
+	}
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sparse Graphs: Matrix Transpose
+# MPI: Matrix Transpose, Sparse Graphs
 
 ## Introductions
 Given a linked list with only the non-zero elements of a sparse matrix and their position in the matrix (the row and the column), creates and outputs the transpose of the original matrix. There will be only one process with rank 0, that will read the file name with the input data, read from the file the linked list
