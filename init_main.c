a693f152ea2c779443cf15555fc1c0981275e65d
BillyD <bdang12@gmail.com>
2017-12-20 10:11:14 -0800

    needs transpose ops now

--- init_main.c
+++ init_main.c
@@ -9,7 +9,6 @@ Description: Sparse Matrix, Matrix Tanspose
 Sources:
 
 Linked List - http://www.zentut.com/c-tutorial/c-linked-list/
-2D Dyanmic Arrays in C - http://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
 
 Docker MPI-Image
 docker run --rm -it -v $(pwd):/project nlknguyen/alpine-mpich
@@ -23,12 +22,26 @@ docker run --rm -it -v $(pwd):/project nlknguyen/alpine-mpich
 //#include <time.h>
 
 
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+
 typedef struct node
 {
     int i, j, value;
     struct node *next;
 } node;
 
+
+
+//used with MPI
+typedef struct nodeArr
+{
+    int i_Val, j_Val, v_Val;
+
+}nodeArr;
 
 typedef void (*callback_1)(node* value);
 
@@ -103,6 +116,7 @@ int main(int argc, char *argv[])
     //linked list
     node*head = NULL;
 
+
     //variables used to help store values into nodes
     int num_insert = 0;
     int row_i = 0;
@@ -112,24 +126,14 @@ int main(int argc, char *argv[])
     //file read in operations
     int m_Row = 8;
     int m_Col = 6;
+    int f_row = 0;
     int curr_Col_count = 0;
     const char delimiter[1] = " ";
     int num_Nodes = 0;
-    const int BUFFER = 100; 
-    char line[BUFFER];
-
-
-    //finding max of i's and j's + store values into original matrix
-    int temp_i, temp_j, max_Row, max_Col, temp_Val;
-    int index = 0;
-
 
 
     //variables for MPI, some values assign are temps
     //NOTE: May need to change some values
-
-
-
     int rank;
     int size;
 
@@ -163,7 +167,12 @@ int main(int argc, char *argv[])
             printf("Cannot open file \n") ;
             exit(0);
         }
+
+        //Read in the size of the matrix
+        const int BUFFER = 100;
+        char line[BUFFER];
         
+
         //get number of rows
         m_Row = atoi(fgets(line,BUFFER,fp));
 
@@ -171,8 +180,8 @@ int main(int argc, char *argv[])
         m_Col = atoi(fgets(line, BUFFER, fp));
 
         //TEST: print number of rows and cols
-        printf("Number of Rows %d\n", m_Row);
-        printf("Num of Col %d\n", m_Col);
+        printf("Num Row %d\n", m_Row);
+        printf("Num Col %d\n", m_Col);
 
    
         //READ FROM FILE
@@ -222,140 +231,93 @@ int main(int argc, char *argv[])
         //Close the file
         fclose(fp);
 
-
-        //print the number of nodes created
-        printf("Number of Nodes Created %d\n", num_Nodes);
-
         //print the linked list
         print(head);
 
 
-        //pointer to head, assist in reading values from linked list into our designated arrays
-        node*reader = head;
-
-
-        //variables for finding dimension of matrix
-        int *i_Array = malloc(num_Nodes*sizeof(int));
-        int *j_Array = malloc(num_Nodes*sizeof(int));
-
-        //need to put the head into struct
-        while (reader != NULL )
-        {
-            //reader->value;
-            temp_i = reader->i;
-            temp_j = reader->j;
-
-            if(index != num_Nodes)
-            {
-                i_Array[index] = temp_i;
-                j_Array[index] = temp_j;
-            }
-
-            index++;
-            reader = reader->next;
-
-        }
-        //set to null to dereference pointer
-        reader = NULL;
-
-        //Need to determine max of i and j for size of matrix
-        max_Row = i_Array[0];
-        max_Col = j_Array[0];
-
-        for(int i = 0; i < num_Nodes; i++)
-        {
-            if(max_Row < i_Array[i])
-            {
-                max_Row = i_Array[i];
-            }
-
-             if(max_Col < j_Array[i])
-            {
-                max_Col = j_Array[i];
-            }
-            
-        }
-
-        //add 1 to get the actual size dimension of matrix
-        max_Row += 1;
-        max_Col += 1;
-
-        //print the dimension of i and j
-        printf("Max Row %d\n", max_Row);
-        printf("Max Col %d\n", max_Col);
-
-       
-        //Create the origianl matrix and allocate space, intitalize and set values to 0
-        int *matrix = (int*) malloc(max_Row * max_Col * sizeof(int));
-        for(int i = 0; i < max_Row; i++)
-            {
-                for(int j = 0; j < max_Col; j++)
-                {   
-                    *(matrix + i*max_Col + j) = 0;   
-                }
-            }
-
-        
-        //reuse some of the old variables to help store values into original matrix
-        temp_Val = 0;
-        temp_i = 0;
-        temp_j = 0;
-
-        //Note: Set reader back to head
-        reader = head;
-
-        while (reader != NULL)
-        {
-            temp_Val = reader->value;
-            temp_i = reader->i;
-            temp_j = reader->j;
-
-
-            for(int i = 0; i < max_Row; i++)
-            {
-                for(int j = 0; j < max_Col; j++)
-                {
-                    //if coordinates match, then place value into corresponding spot
-                    //in matrix
-                    if(temp_i == i && temp_j == j)
-                    {
-                        *(matrix + i*max_Col + j) = temp_Val;
-                    }
-                }
-            }
-            
-            //Traverse to the next node in linked list
-            reader = reader->next;
-
-        }
-
-        //Again dereference reader 
-        reader = NULL;
-
-
-        printf("\nOriginal Matrix\n\n");
-        for(int i = 0; i < max_Row; i++)
-        {
-            for(int j = 0; j < max_Col; j++)
-            {
-                printf("%d  ",  *(matrix + i*max_Col + j));
-                if (j == max_Col - 1)
-                {
-                    printf("\n\n");
-                }
-            }
-
-        }
-    
-
-
-    } //end of process 0
+    }
 
     //-----------------------HERE: Main MPI Operation-----------------------
 
-   
+    //specify MPI structure
+    int blocks[3] = {1,1,1};
+    MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
+    MPI_Aint displacements[3];
+
+    MPI_Datatype n_NodeObj;
+    MPI_Aint intex;
+
+    MPI_Type_extent(MPI_INT, &intex);
+    displacements[0] = 0;
+    displacements[1] = intex;
+    displacements[2] = intex + intex;
 
 
+    MPI_Type_struct(3, blocks, displacements, types, &n_NodeObj);
+    MPI_Type_commit(&n_NodeObj);
+
+
+    //create an array that will hold values in linked for other processes
+    //all processes made the same array
+    nodeArr *holder;
+    holder = (nodeArr *) malloc(num_Nodes);
+
+
+    //array for the recv buffer
+    nodeArr rec_buff[200];
+
+    //control variable for storing the value into the nodeArr
+    int index = 0;
+
+    //pointer to head, used to read and store values in NodeArr struct
+    node*reader = head;
+
+    //need to put the head into struct
+    while (reader != NULL )
+    {
+        holder[index].v_Val = reader->value;
+        holder[index].i_Val = reader->i;
+        holder[index].j_Val = reader->j;
+
+        index++;
+        reader = reader->next;
+
+    }
+
+    //set ptr to null
+    reader = NULL;
+
+
+
+    int rem = num_Nodes % size;
+    int*sendcount = malloc(sizeof(int)*size);
+    int*displs = malloc(sizeof(int)*size);
+    int sum = 0;
+
+
+    //calculate "chunk size" send counts and displacements
+    for (int i = 0; i < size; i++)
+    {
+        sendcount[i] = num_Nodes/size;
+        if(rem > 0)
+        {
+            sendcount[i]++;
+            rem--;
+        }
+
+        displs[i] = sum;
+        sum += sendcount[i];
+    }
+
+
+    //see what value is being sent
+    if (0 == rank)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            printf("sendcount[%d] = %d\tdispls[%d] = %d\n", i, sendcount[i], i, displs[i]);
+        }
+    }
 
     MPI_Finalize();
 
@@ -366,9 +328,8 @@ int main(int argc, char *argv[])
     {
         printf("\n------ Deallocating Memory------\n");
         dispose(head);
-       
-        //free(sendcount);
-        //free(displs);
+        free(sendcount);
+        free(displs);
         printf("All done!\n");
     }
 
@@ -376,32 +337,3 @@ int main(int argc, char *argv[])
 
 }
 
-
-
-/*------------------------Output---------------------------
-/project/Documents/GitHub/474-Project-2 $ mpirun -n 2 ./test
-Number of Rows 4
-Num of Col 3
-Number of Nodes Created 4
-11 1 1
-45 1 0
-23 0 1
-22 0 0
-Max Row 2
-Max Col 2
-
-Original Matrix
-
-22  23
-
-45  11
-
-
------- Deallocating Memory------
-All done!
-/project/Documents/GitHub/474-Project-2 $
-
-
-
-
------------------------------------------------------------*/
